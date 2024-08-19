d7268edf3fc3aaf950124960a93f043612c4b726
Lê Tiến Đạt <DatLT.B21VT013@stu.ptit.edu.vn>
2024-08-19 08:56:10 +0700

    final

--- validate/admin/product.validate.js
+++
@@ -1,8 +0,0 @@
-module.exports.createPost =(req,res,next) =>{
-    if(!req.body.title){
-        req.flash('info','Vui lòng nhập tên sản phẩm');
-        res.redirect("back");
-        return;
-    }
-    next();
-} 
